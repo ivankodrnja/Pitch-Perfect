a2cb164607d948eafd59ab6270857e3b15c9f5ea
Ivan Kodrnja <ikodrnja@gmail.com>
2015-03-08 13:55:37 +0100

    add record button, disable record button when stop button is pressed

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        //hide the stop button
+        stopButton.hidden = true
+        
+        recordButton.enabled = true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        recordingLabel.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func recordAudio(sender: UIButton) {
+    
+        recordingLabel.hidden = false
+        
+        stopButton.hidden = false
+        
+        recordButton.enabled = false
+        
+        // recordingLabel.hidden =  recordingLabel.hidden ? false : true
+        
+        /*
+        if(recordingLabel.hidden){
+            recordingLabel.hidden = false
+        }else{
+            recordingLabel.hidden = true
+        }
+        */
+        
+        //TODO: Record the users's voice
+        
+    
+    }
+    
+  
+    @IBAction func stopAudio(sender: UIButton) {
+        
+        recordingLabel.hidden = true
+    }
 
 }
 
