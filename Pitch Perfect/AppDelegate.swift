d7f745bb59380b29315a184fe9f9fc585efeb444
ivankodrnja <ikodrnja@gmail.com>
2020-12-19 19:38:39 +0100

    edited copyright notice

--- Pitch Perfect/AppDelegate.swift
+++ Pitch Perfect/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  Pitch Perfect
 //
 //  Created by Ivan Kodrnja on 06/03/15.
-//  Copyright (c) 2015-2021 Ivan Kodrnja. All rights reserved.
+//  Copyright (c) 2015-2021 Ivan Kodrnja All rights reserved.
 //
 
 import UIKit
